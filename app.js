a7fdf6b2e4baedd776354ac7beb189f4b158ca42
olethomas93 <norbye93@gmail.com>
2022-11-27 22:41:08 +0100

    param

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ res.send(result)
 
 app.get('/aurora/forecast',cors(corsOptions),function(req,res){
 
-request.get({url:`https://www.yr.no/api/v0/locations/1-${req.body.placenumber}/auroraforecast?language=nb`,json:true},function(err,response,data){
+
+request.get({url:`https://www.yr.no/api/v0/locations/1-${req.query.placenumber}/auroraforecast?language=nb`,json:true},function(err,response,data){
 
 if(err){
 res.send('error')
