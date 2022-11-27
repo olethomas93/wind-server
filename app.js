5d364f10f49fb537118947c67235b2ee3e589fcc
olethomas93 <norbye93@gmail.com>
2022-11-27 21:20:47 +0100

    fdf

--- app.js
+++ app.js
@@ -46,7 +46,12 @@ var coords= data.coordinates
 var result=[]
 for(var i in coords){
 
-	result.push([Number(coords[i][1]) || 0 ,Number(coords[i][0]) || 0, Number(coords[i][2])/100 || 0])
+	if(coords[i][2]>0){
+
+		result.push([Number(coords[i][1]) || 0 ,Number(coords[i][0]) || 0, Number(coords[i][2])/100 || 0])
+	}
+
+	
 
 
 }
