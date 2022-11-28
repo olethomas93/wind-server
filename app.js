8c62d2b364a04fa1e6b1f5a2c2fb19a5fa058540
olethomas93 <norbye93@gmail.com>
2022-11-28 21:31:42 +0100

    fef

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ res.send(data)
 app.get('/place',cors(corsOptions),function(req,res){
 
 
-	request.get({url:`https://www.yr.no/api/v0/locations/search?language=nb&lat=${req.query.lat}&lon=${req.query.lng}&accuracy=1000`,json:true},function(err,response,data){
+	request.get({url:`https://www.yr.no/api/v0/locations/search?language=nb&lat=${req.query.lat}&lon=${req.query.lng}&accuracy=500`,json:true},function(err,response,data){
 	
 	if(err){
 	res.send('error')
